bd73293a5e1dd573c239272bc10a436e103c57c5
unknown <suhasmanjunath2005.mail@gmail.com>
2025-01-26 10:43:08 +0530

    all links and improments added

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import requests
+from send_email import send_email
 
+topic = "tesla"
 api_key = "9e11f332980c4e58b21e4188e4cac500"
-url = "https://newsapi.org/v2/everything?q=tesla&"\
+url = f"https://newsapi.org/v2/everything?q={topic}&"\
     "from=2024-12-25&sortBy=publishedAt&apiKey="\
-        "9e11f332980c4e58b21e4188e4cac500"
+        "9e11f332980c4e58b21e4188e4cac500&language=en"
 
 request = requests.get(url)
 
 content = request.json()
 
-for articles in content["articles"]:
-    print(articles["title"])
-    print(articles["description"])
+body = "Subject: Today's News\n\n" 
+for article in content["articles"][0:20]:
+    if article["title"] is not None:
+        body = body + article["title"]+"\n"+article["description"]+"\n"+article["url"]+ 2*"\n"
+
+body = body.encode("utf-8")
+send_email(body)
+
+   
